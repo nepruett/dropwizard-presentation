56faff5c4177c477e3f9d5ef91bbf74b6405856a
npruett <nathan.pruett@gmail.com>
2015-06-26 23:33:10 -0400

    HelloWorld DropWizard project + reveal.js presentation submodule

---
+++ src/main/java/com/blue_star_software/HelloWorldApplication.java
@@ -0,0 +1,38 @@
+package com.blue_star_software;
+
+import com.blue_star_software.health.TemplateHealthCheck;
+import com.blue_star_software.resource.HelloWorldResource;
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/assets/", "/", "index.html"));
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+    }
+
+}
